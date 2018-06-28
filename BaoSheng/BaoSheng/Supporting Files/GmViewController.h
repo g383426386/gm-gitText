21be7d755cec317bfe4ef59650591d3ac19a869c
gongmaoli <383426386@qq.com>
2018-06-28 23:17:32 +0800

    dev-commit

---
+++ BaoSheng/BaoSheng/Supporting Files/GmViewController.h
@@ -0,0 +1,13 @@
+//
+//  GmViewController.h
+//  BaoSheng
+//
+//  Created by GML on 2018/6/28.
+//  Copyright © 2018年 haozheng. All rights reserved.
+//
+
+#import "BSBaseControl.h"
+
+@interface GmViewController : BSBaseControl
+
+@end
